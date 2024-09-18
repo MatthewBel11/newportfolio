78d443b9b6a40fa72e0f8122bf56834b7c11daa1
c2026706 <c2026706@ncl.ac.uk>
2024-09-18 05:58:05 +0100

    - Added text to AboutMe

--- my-app/src/Pages/AboutPage/AboutPage.js
+++ my-app/src/Pages/AboutPage/AboutPage.js
@@ -4,12 +4,15 @@ import Documents from '../../components/Documents/Documents.js'
 import Work from '../../components/Work/WorkExp.js'
 import styles from './AboutPage.module.css'
 
-var text = 'Basic holder text'
+var text = "A driven, resilient software engineering student at Newcastle University with a passion for coding and camping. Having successfully completed two summer internships, gaining a solid foundation in the Financial Services and Technology  industry whilst developing knowledge and softer skills within the development and architecture space. "
+
+var text2 = " As I begin the third year of a Software Engineering Course, I’m focussed on further developing my problem-solving, coding and collaborative skills  through hackathons, personal projects and focussed learning whilst continuing to  challenge myself and explore new technologies. "
+
 
 function AboutPage() {
   return (
     <>
-      <PrimaryText title='About Me' text={text}></PrimaryText>
+      <PrimaryText title='About Me' text={text} text2={text2}> </PrimaryText>
       <Documents/>
       <Work/>
     </>
--- my-app/src/components/PrimaryText/PrimaryText.js
+++ my-app/src/components/PrimaryText/PrimaryText.js
@@ -7,7 +7,9 @@ function PrimaryText(props){
     return(
         <div className={styles.TextHolder}>
             <h2 className={styles.TextTitle}> { props.title } </h2>
-            <p className={styles.TextBlock}> { props.text } </p>
+            <p className={styles.TextBlock}>
+            {props.text} <br /> <br /> {props.text2}
+            </p>
         </div>
     )
 }
