edb28ad3b7015eae77c0cdc775a274991320852d
c2026706 <c2026706@ncl.ac.uk>
2024-09-18 05:45:08 +0100

    - Added documents section with pdfs

---
+++ my-app/src/components/Documents/Documents.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './Documents.module.css'
+import transcriptOne from './YearOneTranscript.pdf'
+import transcriptTwo from './YearTwoTranscript.pdf';
+
+function Documents(){
+
+    return(
+        <div className={styles.MainContainer}>
+            <div className={styles.TitleHold}>
+                <h2 className={styles.DocumentsTitle}> Documents </h2>
+            </div>
+            <div className={styles.DocumentHolder}>
+                
+                <div className={styles.LeftSide}>
+                    <h3 className={styles.DocumentsSub}> University Transcripts </h3>
+                        <p className={styles.DocumentRef}><a  className={styles.Document} href={transcriptTwo} target="_blank" rel="noopener noreferrer">
+                            Stage 2 Transcript   </a> Last updated: 18/09/2024 </p>
+                        
+                        <p className={styles.DocumentRef}><a  className={styles.Document} href={transcriptOne} target="_blank" rel="noopener noreferrer">
+                            Stage 1 Transcript   </a> Last updated: 18/09/2024 </p>
+                        
+                    
+    
+
+                </div>
+
+                <div className={styles.RightSide}>
+                    <h3 className={styles.DocumentsSub}> Professional </h3>
+                    <p className={styles.DocumentRef}><a  className={styles.Document} href={transcriptTwo} target="_blank" rel="noopener noreferrer">
+                            Resume   </a> Last updated: 18/09/2024 </p>
+                        
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Documents;
