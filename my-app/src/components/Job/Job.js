cbe1bfa593b1e66ce9a0026cab275d322d3e1f89
c2026706 <c2026706@ncl.ac.uk>
2024-09-20 23:59:45 +0100

    - Minor changes added

--- my-app/src/components/Job/Job.js
+++ my-app/src/components/Job/Job.js
@@ -5,9 +5,19 @@ function Job(props){
 
     return(
         <>
-        <h2 className={styles.CompanyTitle}>{props.CompanyTitle}</h2>
-        <h2 className={styles.JobTitle}>{props.JobTitle}</h2>
-        <h3 className={styles.JobLocation}>{props.JobLocation}</h3>
+        <div className={styles.Container}>
+
+            <div className={styles.ImageContainer}>
+                <img className={styles.Image} alt='JobLogo' src={props.Image} />
+            </div>
+
+            <div className={styles.JobInfo}>
+                <h2 className={styles.CompanyTitle}>{props.CompanyTitle}</h2>
+                <h2 className={styles.JobTitle}>{props.JobTitle}</h2>
+                <h3 className={styles.JobLocation}>{props.JobLocation}</h3>
+            </div>
+
+        </div>
         <p className={styles.JobText}>{props.JobText}</p>
 
         </>
