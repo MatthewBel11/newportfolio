7a15c05e2db022cd9dab536d11bb47a21a20f9cb
c2026706 <c2026706@ncl.ac.uk>
2024-09-21 22:51:44 +0100

    - Added the skill section to main page

--- my-app/src/Pages/AboutPage/AboutPage.js
+++ my-app/src/Pages/AboutPage/AboutPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PrimaryText from '../../components/PrimaryText/PrimaryText.js'
 import Documents from '../../components/Documents/Documents.js'
 import Work from '../../components/Work/WorkExp.js'
+import SkillStack from '../../components/SkillStack/SkillStack.js'
 import styles from './AboutPage.module.css'
 
 var text = "A driven, resilient software engineering student at Newcastle University with a passion for coding and camping. Having successfully completed two summer internships, gaining a solid foundation in the Financial Services and Technology  industry whilst developing knowledge and softer skills within the development and architecture space. "
@@ -15,6 +16,7 @@ function AboutPage() {
       <PrimaryText title='About Me' text={text} text2={text2}> </PrimaryText>
       <Documents/>
       <Work/>
+      <SkillStack/>
     </>
   );
 }
