f5d01d66fc7b574ed9c20d64ff998618237ae379
c2026706 <c2026706@ncl.ac.uk>
2024-09-21 22:50:58 +0100

    - Made skill part of page

---
+++ my-app/src/components/SkillStack/SkillStack.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './SkillStack.module.css'
+
+import PythonIcon from './Python.png'
+import CSharp from './c-sharp.png'
+import html from './html.png'
+import JavaS from './java-script.png'
+import Java from './java.png'
+import Sqlite from './sql-server.png'
+
+import Skill from '../Skills/Skills.js'
+
+function Skills(props){
+
+    /*
+    Skills needed:
+
+    Python, Java, C#, JavaScript
+    HTML CSS Tailwind, React, Vue, Astro
+    REST, Firebase
+    
+    */
+
+    return(
+        <>
+        <div className={styles.TitleContainer}>
+            <h2 className={styles.Title}> Skills </h2>
+        </div>
+        <div className={styles.Container}>
+            <Skill Icon={PythonIcon} SkillName='Python'/>
+            <Skill Icon={CSharp} SkillName='C#'/>
+            <Skill Icon={html} SkillName='Html'/>
+            <Skill Icon={JavaS} SkillName='JavaScript'/>
+            <Skill Icon={Java} SkillName='Java'/>
+            <Skill Icon={Sqlite} SkillName='Sqlite'/>
+        </div>
+        </>
+    )
+}
+
+export default Skills;   
