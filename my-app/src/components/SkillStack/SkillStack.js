90db3db70b6a20588940c54c01b36e225abb9da2
c2026706 <c2026706@ncl.ac.uk>
2024-09-23 05:11:13 +0100

    - Added more skills to bottom of page

--- my-app/src/components/SkillStack/SkillStack.js
+++ my-app/src/components/SkillStack/SkillStack.js
@@ -7,19 +7,16 @@ import html from './html.png'
 import JavaS from './java-script.png'
 import Java from './java.png'
 import Sqlite from './sql-server.png'
+import astro from './astro.png'
+import firebase from './firebase.png'
+import ReactImg from './React.png'
+import Vue from './vue.png'
+import css from './css.png'
+
 
 import Skill from '../Skills/Skills.js'
 
 function Skills(props){
-
-    /*
-    Skills needed:
-
-    Python, Java, C#, JavaScript
-    HTML CSS Tailwind, React, Vue, Astro
-    REST, Firebase
-    
-    */
 
     return(
         <>
@@ -33,6 +30,11 @@ function Skills(props){
             <Skill Icon={JavaS} SkillName='JavaScript'/>
             <Skill Icon={Java} SkillName='Java'/>
             <Skill Icon={Sqlite} SkillName='Sqlite'/>
+            <Skill Icon={firebase} SkillName='Firebase'/>
+            <Skill Icon={astro} SkillName='Astro'/>
+            <Skill Icon={Vue} SkillName='Vue'/>
+            <Skill Icon={ReactImg} SkillName='React'/>
+            <Skill Icon={css} SkillName='CSS'/>
         </div>
         </>
     )
