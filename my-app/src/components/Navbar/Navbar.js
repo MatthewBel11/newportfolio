9076c1b3d25773d873bf77f517f845d8abefae29
c2026706 <c2026706@ncl.ac.uk>
2024-09-24 19:44:50 +0100

    - Added styling to current page element

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState('/');
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
+  };
+
   return (
     <div className={styles.Navholder}>
-      <Link to="/" className={styles.LinkElement}> Home </Link>
-      <Link to="/projects" className={styles.LinkElement}> Projects </Link>
-      <Link to="/contact" className={styles.LinkElement}> Contact </Link>
+      <Link
+        to="/"
+        className={`${styles.LinkElement} ${activeLink === '/' ? styles.Active : ''}`}
+        onClick={() => handleLinkClick('/')}
+      >
+        Home
+      </Link>
+      <Link
+        to="/projects"
+        className={`${styles.LinkElement} ${activeLink === '/projects' ? styles.Active : ''}`}
+        onClick={() => handleLinkClick('/projects')}
+      >
+        Projects
+      </Link>
+      <Link
+        to="/contact"
+        className={`${styles.LinkElement} ${activeLink === '/contact' ? styles.Active : ''}`}
+        onClick={() => handleLinkClick('/contact')}
+      >
+        Contact
+      </Link>
     </div>
   );
 }
