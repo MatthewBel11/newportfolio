7de517df313a8f6da5a321d839e16abacb792d5c
c2026706 <c2026706@ncl.ac.uk>
2024-09-24 19:44:36 +0100

    - Updated app to handle unknown urls & Contact page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,28 +4,26 @@ import './App.css';
 import TitleCard from './components/Titlecard/Titlecard.js';
 import Navbar from './components/Navbar/Navbar.js';
 import AboutPage from './Pages/AboutPage/AboutPage.js';
-import ProjectsPage from './Pages/ProjectsPage/ProjectsPage.js'
-
+import ProjectsPage from './Pages/ProjectsPage/ProjectsPage.js';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage.js'; // Import the 404 Page component
+import ContactPage from './Pages/ContactPage/Contact.js';
 
 function App() {
   return (
     <Router>
       <div className='Background'>
-
-        
         <div className='ContentColumn'>
           <TitleCard />
           <Navbar />
           
-          {/* Define Routes for each page */}
           <Routes>
             <Route path="/" element={<AboutPage />} />
             <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/contact" element={''} />
+            <Route path="/contact" element={<ContactPage />} />
+            
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
-
-
       </div>
     </Router>
   );
